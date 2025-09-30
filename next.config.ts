13b93e7c2e7d57083c8f8f9d1e39f61f658cb0c5
sfrunza <frunza.sergiu3@gmail.com>
2025-09-30 16:32:37 -0400

    vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,3 @@ const withMDX = createMDX({
 
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig);
-
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
